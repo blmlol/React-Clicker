85eb0b7cf9d13c2aeb72c9cc2d6778d95317abc5
Davey Wong <notwdjw@gmail.com>
2020-03-01 19:57:00 -0800

    Updates

---
+++ precache-manifest.144506c5ae913285603bdc79efdd5aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52061e88411d8436b77335543930b6e",
+    "url": "/React-Clicker/index.html"
+  },
+  {
+    "revision": "99b787a8d184dedf1498",
+    "url": "/React-Clicker/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "b54fa6947c55ed8941de",
+    "url": "/React-Clicker/static/js/2.1ee643ad.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Clicker/static/js/2.1ee643ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b787a8d184dedf1498",
+    "url": "/React-Clicker/static/js/main.d167bb55.chunk.js"
+  },
+  {
+    "revision": "ecedde2692015a9340a1",
+    "url": "/React-Clicker/static/js/runtime-main.6f6f0c7e.js"
+  }
+]);
